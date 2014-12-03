4483331c297045db633ea9c7186fd0d75b48ab81
Raushan Alleyne <raushan802@gmail.com>
2014-12-02 21:49:51 -0500

    changed so you dont have to pass in url key in post data when asking for nearby coordinates. bug fixes

--- views.py
+++ views.py
@@ -7,13 +7,13 @@ from django.http import HttpResponseRedirect
 from form import UserImageForm
 from models import UserImage
 from decimal import Decimal
+import json
 
 def index(request):
 	if request.method == 'POST':
 		post_data = request.POST
 		lat = Decimal(post_data['latitude'])
 		lon = Decimal(post_data['longitude'])
-		url = post_data['image_url']
 		try:
 			#check for the key that will by passed our android application
 			if post_data['source']:
@@ -22,20 +22,22 @@ def index(request):
 				#the app engine non-relational database has a limit on joins
 				results_lat = UserImage.objects.filter(latitude__gte=(lat - range_val),
 										 latitude__lte=(lat + range_val))
-				results_lon = UserImage.objects.filter(longitude__gte=(lat - range_val),
-										 longitude__lte=(lat + range_val))
+				results_lon = UserImage.objects.filter(longitude__gte=(lon - range_val),
+										 longitude__lte=(lon + range_val))
 				#take the common objects between the 2 sets
 				joined_results =  set(results_lat) & set(results_lon)
 				json_data = serializers.serialize("json", joined_results)
 				return HttpResponse(json_data,mimetype='application/json')	
 		except MultiValueDictKeyError:
+			url = post_data['image_url']
 			#populate model with data and save to database
 			a = UserImage()
 			a.longitude = lon
 			a.latitude = lat
 			a.image_url = url
 			a.save()
-			return HttpResponseRedirect('/posted')
+			json_data = json.dumps({'success':True, 'status':HttpResponse.status_code})
+			return HttpResponse(json_data,mimetype='application/json')	
 	ctx = {}	
 	ctx['form'] = UserImageForm
 	return render_to_response('main.html', ctx, context_instance=RequestContext(request))
