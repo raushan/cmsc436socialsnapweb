4019fc35aa6c19c62454665cc0ea596f4d44ce10
Raushan Alleyne <raushan802@gmail.com>
2014-12-04 13:20:33 -0500

    more decimal places

--- models.py
+++ models.py
@@ -1,8 +1,8 @@
 from django.db import models
 
 class UserImage(models.Model):
-	latitude = models.DecimalField(null=True,max_digits=10,decimal_places=3)
-	longitude = models.DecimalField(null=True, max_digits=10,decimal_places=3)
+	latitude = models.DecimalField(null=True,max_digits=10,decimal_places=4)
+	longitude = models.DecimalField(null=True, max_digits=10,decimal_places=4)
 	image_url = models.CharField(null=True)
 
 	class Meta:
